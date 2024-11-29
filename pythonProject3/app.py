018f7d75df51eccebd555ad826ee657f12ad59fb
mohammad-aslam <mohammadaslam62819@gmail.com>
2024-11-29 16:47:54 +0530

    Add files via upload

---
+++ pythonProject3/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app =  Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/help')
+def help():
+    return render_template('help.html')
+
+if __name__ =='__main__':
+    app.run(host='127.0.0.1',port=5003,debug=True)
